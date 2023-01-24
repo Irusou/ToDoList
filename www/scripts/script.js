55ad3d07d9be325fd4bcfa6b3e92e997b92d727a
João Pedro <irus0ubusiness@gmail.com>
2023-01-24 20:55:21 +0000

    style update, add/remove function update
    redesign of the main page.
    btnAdd function almost complete.
    btnRemove not working properly.
    helper function not working properly.

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,25 +1,36 @@
 var init = function(){
-    const formItem = document.getElementById('itemToAdd');
+    const itemToAdd = document.getElementById('itemToAdd');
     const btnAdd = document.getElementById('btnAddItem');
     const btnRemove = document.getElementById('btnRemoveItem');
+    const itemsList = document.getElementById('list');
     
 
     btnAdd.addEventListener('click', function(evt){
-        if (formItem !== ""){
-
-        }else{
+        if (itemToAdd.value === ""){
             alert("Please enter a valid item!");
+            return ;
         }
+        let newItem = document.createElement('li');
+        newItem.appendChild(document.createTextNode(itemToAdd.value));
+        itemsList.append(newItem);
     });
 
     btnRemove.addEventListener('click', function(evt){
-        let listItems = document.getElementsByTagName('li');
-        for (const item in listItems) {
-            if (item === formItem) {
-
-            }
-        }
+        let node = itemsList[itemToAdd.value];
+        itemsList.remove(node);
     });
 }
 
-document.onload = init;
+/**
+ * checks if an item is already on the list
+ */
+function checkIfItemExists(itemsList){
+    for (const li in itemsList) {
+        if (li.value === itemsList.value){
+            alert("Item already on list!");
+        }
+    }
+    return false;
+}
+
+window.onload = init;
